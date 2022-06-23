0d374551de5695640c184aee0808a73b005d869e
Hemanth2327 <justinraj136@gmail.com>
2022-06-23 15:34:57 +0530

    Done

---
+++ src/com/test/javaProject.java
@@ -0,0 +1,13 @@
+package com.test;
+
+public class javaProject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("hey guys");
+		
+		System.out.println("see you around");
+	}
+
+}
