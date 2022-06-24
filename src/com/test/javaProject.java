009624856fb77ec5878fc66f6565368228aef0fa
Hemanth2327 <justinraj136@gmail.com>
2022-06-24 17:58:12 +0530

    H Updated

--- src/com/test/javaProject.java
+++ src/com/test/javaProject.java
@@ -3,11 +3,15 @@ package com.test;
 public class javaProject {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		
 		System.out.println("hey guys");
 		
 		System.out.println("see you around");
+		
+		String a="Hemanth";
+		
+		System.out.println(a);
 	}
 
 }
